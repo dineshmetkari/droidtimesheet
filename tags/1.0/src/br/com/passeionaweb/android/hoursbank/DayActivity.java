8d322fac0f5db2593ebb479eeacdeee5f5502eb4
panthro.rafael <panthro.rafael@bee5d759-e3f5-d037-51d3-742891c3956c>
2010-08-03 21:42:50 +0000

    first release

---
+++ tags/1.0/src/br/com/passeionaweb/android/hoursbank/DayActivity.java
@@ -0,0 +1,102 @@
+package br.com.passeionaweb.android.hoursbank;
+
+import java.util.Calendar;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class DayActivity extends CheckpointListActivity {
+
+	private long hoursDone;
+	private long minHours;
+
+	protected TimePickerDialog.OnTimeSetListener onEditCheckpointListener = new OnTimeSetListener() {
+
+		@Override
+		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+			Calendar cal = Calendar.getInstance();
+			cal.set(Calendar.HOUR_OF_DAY, hourOfDay);
+			cal.set(Calendar.MINUTE, minute);
+			editCheckpoint(cal.getTimeInMillis());
+		}
+	};
+
+	private boolean areHoursByDayDone() {
+		return hoursDone > minHours;
+	}
+
+	@Override
+	protected Dialog createEditDialog() {
+		Calendar c = Calendar.getInstance();
+		TimePickerDialog editDialog = new TimePickerDialog(this, onEditCheckpointListener, c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE), true);
+		editDialog.setTitle(R.string.dialog_add_checkpoint_title);
+		return editDialog;
+	}
+
+	protected void fillData() {
+
+		db.open();
+		// Get all of the rows from the database and create the item list
+		Cursor cursor = db.getCheckpointsByDay(Calendar.getInstance());
+		startManagingCursor(cursor);
+
+		CheckpointCursorAdapter adapter = new CheckpointCursorAdapter(this, CheckpointCursorAdapter.DAY, cursor);
+		setListAdapter(adapter);
+
+		if (cursor.getCount() > 0) {
+			findViewById(R.id.layoutContainer).setVisibility(View.VISIBLE);
+			hoursDone = chk.calculateTotalHours(cursor);
+			String totalHours = chk.formatTotalHours(hoursDone);
+			((TextView) findViewById(R.id.lblTotalHours)).setText(totalHours);
+			if (areHoursByDayDone()) {
+				((ImageView) findViewById(R.id.imgHoursDone)).setImageResource(R.drawable.ic_btn_round_plus);
+			} else {
+				((ImageView) findViewById(R.id.imgHoursDone)).setImageResource(R.drawable.ic_btn_round_minus);
+			}
+		}
+		db.close();
+	}
+
+	@Override
+	protected long calculateMinHoursByPref() {
+		return chk.unformatTotalHours(chk.getHoursPrefByDay(Calendar.getInstance().get(Calendar.DAY_OF_WEEK)));
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		minHours = calculateMinHoursByPref();
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		minHours = calculateMinHoursByPref();
+	}
+
+	@Override
+	protected Dialog createAddDialog() {
+		Calendar c = Calendar.getInstance();
+		TimePickerDialog addDialog = new TimePickerDialog(this, onAddCheckpointListener, c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE), true);
+		addDialog.setTitle(R.string.dialog_add_checkpoint_title);
+		return addDialog;
+	}
+
+	protected TimePickerDialog.OnTimeSetListener onAddCheckpointListener = new OnTimeSetListener() {
+
+		@Override
+		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+			Calendar cal = Calendar.getInstance();
+			cal.set(Calendar.HOUR_OF_DAY, hourOfDay);
+			cal.set(Calendar.MINUTE, minute);
+			insertCheckpoint(cal.getTimeInMillis());
+		}
+	};
+}
---
+++ tags/1.0/src/br/com/passeionaweb/android/hoursbank/db/DatabaseHelper.java
@@ -0,0 +1,117 @@
+package br.com.passeionaweb.android.hoursbank.db;
+
+import java.util.Calendar;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+	private static final int DATABASE_VERSION = 1;
+	private static final String DATABASE_NAME = "droidtimesheetdata";
+	private static final String TABLE_NAME = "CheckPoints";
+	public static final String KEY_ID = "_id";
+	public static final String KEY_CHECKPOINT = "checkpoint";
+	private static final String CREATE_SQL = "create table if not exists " + TABLE_NAME + " (" + KEY_ID + " integer primary key autoincrement ," + KEY_CHECKPOINT + " datetime not null)";
+	public static final String STATUS_IN = "STATUS_IN";
+	public static final String STATUS_OUT = "STATUS_OUT";
+
+	private SQLiteDatabase db;
+
+	public DatabaseHelper(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+	}
+
+	@Override
+	public void onCreate(SQLiteDatabase db) {
+		db.execSQL(CREATE_SQL);
+	}
+
+	public void open() {
+		db = getWritableDatabase();
+	}
+
+	public void close() {
+		db.close();
+	}
+
+	@Override
+	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+		// TODO create the code to upgrade database according with versions
+	}
+
+	public Cursor getCheckpointsByDay(Calendar day) {
+		Calendar startDate = (Calendar) day.clone();
+		Calendar endDate = (Calendar) day.clone();
+
+		startDate.set(Calendar.HOUR_OF_DAY, 0);
+		startDate.set(Calendar.MINUTE, 0);
+		startDate.set(Calendar.SECOND, 0);
+
+		endDate.set(Calendar.HOUR_OF_DAY, 23);
+		endDate.set(Calendar.MINUTE, 59);
+		endDate.set(Calendar.SECOND, 59);
+
+		return db.query(TABLE_NAME, new String[] { KEY_ID, KEY_CHECKPOINT }, KEY_CHECKPOINT + " >= " + startDate.getTimeInMillis() + " AND " + KEY_CHECKPOINT + " <= " + endDate.getTimeInMillis(),
+				null, null, null, KEY_CHECKPOINT);
+
+	}
+
+	public Cursor getMonthCheckpoints() {
+		Calendar calendar = Calendar.getInstance();
+		calendar.set(Calendar.DAY_OF_MONTH, 1);
+		calendar.set(Calendar.HOUR_OF_DAY, 0);
+		calendar.set(Calendar.MINUTE, 0);
+		calendar.set(Calendar.SECOND, 0);
+
+		return db.query(TABLE_NAME, new String[] { KEY_ID, KEY_CHECKPOINT }, KEY_CHECKPOINT + " >= " + calendar.getTimeInMillis(), null, null, null, KEY_CHECKPOINT);
+
+	}
+
+	public Cursor getAllCheckpoints() {
+		return db.query(TABLE_NAME, new String[] { KEY_ID, KEY_CHECKPOINT }, null, null, null, null, KEY_CHECKPOINT);
+	}
+
+	public long insertCheckpoint() {
+		return insertCheckpoint(Calendar.getInstance().getTimeInMillis());
+	}
+
+	public long insertCheckpoint(long checkpoint) {
+		ContentValues values = new ContentValues();
+		values.put(KEY_CHECKPOINT, checkpoint);
+		long result = db.insert(TABLE_NAME, null, values);
+		if (result != -1) {
+			return checkpoint;
+		} else {
+			return result;
+		}
+	}
+
+	public int deleteCheckpoint(long id) {
+		return db.delete(TABLE_NAME, KEY_ID + " = " + id, null);
+	}
+
+	public int editCheckpoint(long id, long timestamp) {
+		ContentValues values = new ContentValues();
+		values.put(KEY_CHECKPOINT, timestamp);
+		return db.update(TABLE_NAME, values, KEY_ID + " = " + id, null);
+	}
+
+	public int updateCheckpoint(long id, long checkpoint) {
+		ContentValues values = new ContentValues();
+		values.put(KEY_CHECKPOINT, checkpoint);
+		return db.update(TABLE_NAME, values, KEY_ID + " = " + id, null);
+	}
+
+	public String getStatus() {
+		int todayCount = getCheckpointsByDay(Calendar.getInstance()).getCount();
+		if (todayCount % 2 == 0) {
+			return STATUS_OUT;
+		} else {
+			return STATUS_IN;
+		}
+	}
+}
