4cbb1474cadccf5afb19ce5f8ccde7f8efb2fa12
panthro.rafael <panthro.rafael@bee5d759-e3f5-d037-51d3-742891c3956c>
2010-08-12 21:08:11 +0000

    release 1.4

---
+++ tags/1.4/src/br/com/passeionaweb/android/hoursbank/PreferencesActivity.java
@@ -0,0 +1,59 @@
+package br.com.passeionaweb.android.hoursbank;
+
+import java.util.Calendar;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.widget.Toast;
+
+public class PreferencesActivity extends PreferenceActivity {
+
+	public static final String KEY_FIRST_DAY = "first_day";
+	public static final String KEY_MIN_LUNCH = "min_lunch";
+	public static final String KEY_HOURS_MONDAY = "hours_monday";
+	public static final String KEY_HOURS_TUESDAY = "hours_tuesday";
+	public static final String KEY_HOURS_WEDNESDAY = "hours_wednesday";
+	public static final String KEY_HOURS_THURSDAY = "hours_thursday";
+	public static final String KEY_HOURS_FRIDAY = "hours_friday";
+	public static final String KEY_HOURS_SATURDAY = "hours_saturday";
+
+	public static int getFirstDayOfMonth(Context context) {
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		return Integer.valueOf(prefs.getString(KEY_FIRST_DAY, "1"));
+	}
+
+	public static String getHoursPrefByDay(Context context, int dayOfWeek) {
+		String defValue = "0:00";
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
+		switch (dayOfWeek) {
+			case Calendar.MONDAY:
+				return prefs.getString(KEY_HOURS_MONDAY, defValue);
+			case Calendar.TUESDAY:
+				return prefs.getString(KEY_HOURS_TUESDAY, defValue);
+			case Calendar.WEDNESDAY:
+				return prefs.getString(KEY_HOURS_WEDNESDAY, defValue);
+			case Calendar.THURSDAY:
+				return prefs.getString(KEY_HOURS_THURSDAY, defValue);
+			case Calendar.FRIDAY:
+				return prefs.getString(KEY_HOURS_FRIDAY, defValue);
+			case Calendar.SATURDAY:
+				return prefs.getString(KEY_HOURS_SATURDAY, defValue);
+		}
+		return defValue;
+	}
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		addPreferencesFromResource(R.xml.settings);
+		if (getIntent().getBooleanExtra("ERROR", false)) {
+			Toast t = Toast.makeText(this, getResources().getString(R.string.prefs_error),
+					Toast.LENGTH_LONG);
+			t.show();
+		}
+	}
+
+}
