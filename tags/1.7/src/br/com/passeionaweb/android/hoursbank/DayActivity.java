9e7802071e298e94e747a1f56b039a9a262e6723
panthro.rafael <panthro.rafael@bee5d759-e3f5-d037-51d3-742891c3956c>
2010-08-25 12:53:03 +0000

    Specific day view selecting the day in month view.

---
+++ tags/1.7/src/br/com/passeionaweb/android/hoursbank/DayActivity.java
@@ -0,0 +1,190 @@
+package br.com.passeionaweb.android.hoursbank;
+
+import java.util.Calendar;
+
+import android.app.Dialog;
+import android.app.TimePickerDialog;
+import android.app.TimePickerDialog.OnTimeSetListener;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Menu;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.TimePicker;
+
+public class DayActivity extends CheckpointListActivity {
+
+	private long hoursDone;
+	private long minHours;
+	private Calendar day;
+	private boolean today = true;
+	public static final int MENU_TODAY = 20;
+
+	protected TimePickerDialog.OnTimeSetListener onEditCheckpointListener = new OnTimeSetListener() {
+
+		@Override
+		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+			Calendar cal = Calendar.getInstance();
+			cal.set(Calendar.HOUR_OF_DAY, hourOfDay);
+			cal.set(Calendar.MINUTE, minute);
+			editCheckpoint(cal.getTimeInMillis());
+		}
+	};
+
+	private boolean areHoursByDayDone() {
+		return hoursDone > minHours;
+	}
+
+	private long getBalance() {
+		if (hoursDone > minHours) {
+			return hoursDone - minHours;
+		} else {
+			return minHours - hoursDone;
+		}
+	}
+
+	@Override
+	protected Dialog createEditDialog() {
+		TimePickerDialog editDialog = new TimePickerDialog(this, onEditCheckpointListener, 0, 0,
+				true);
+		editDialog.setTitle(R.string.dialog_add_checkpoint_title);
+		return editDialog;
+	}
+
+	@Override
+	protected void onPrepareDialog(int id, Dialog dialog) {
+		super.onPrepareDialog(id, dialog);
+		Calendar c = Calendar.getInstance();
+		TimePickerDialog tmDialog = null;
+		switch (id) {
+			case DIALOG_EDIT:
+				db.open();
+				long checkpoint = db.getCheckpointById(editId);
+				db.close();
+				c.setTimeInMillis(checkpoint);
+				tmDialog = (TimePickerDialog) dialog;
+				tmDialog.updateTime(c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE));
+				break;
+			case DIALOG_ADD:
+				tmDialog = (TimePickerDialog) dialog;
+				tmDialog.updateTime(c.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE));
+				break;
+		}
+	}
+
+	protected void fillData() {
+
+		db.open();
+		if (today) {
+			day = Calendar.getInstance();
+		} else {
+			String a = "a";
+		}
+		// Get all of the rows from the database and create the item list
+		Cursor cursor = db.getCheckpointsByDay(day);
+		startManagingCursor(cursor);
+
+		CheckpointCursorAdapter adapter = new CheckpointCursorAdapter(this,
+				CheckpointCursorAdapter.DAY, cursor);
+		setListAdapter(adapter);
+
+		if (cursor.getCount() > 0) {
+			findViewById(R.id.layoutContainer).setVisibility(View.VISIBLE);
+			hoursDone = chk.calculateTotalHours(cursor);
+			String totalHours = chk.formatTotalHours(hoursDone);
+			((TextView) findViewById(R.id.lblTotalHours)).setText(totalHours);
+
+			if (areHoursByDayDone()) {
+				((ImageView) findViewById(R.id.imgHoursBalance))
+						.setImageResource(R.drawable.ic_btn_round_plus);
+			} else {
+				((ImageView) findViewById(R.id.imgHoursBalance))
+						.setImageResource(R.drawable.ic_btn_round_minus);
+			}
+			findViewById(R.id.layoutBalance).setVisibility(View.VISIBLE);
+			((TextView) findViewById(R.id.lblHoursBalance)).setText(chk
+					.formatTotalHours(getBalance()));
+		}
+		db.close();
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		if (getIntent().hasExtra("DAY")) {
+			Calendar day = Calendar.getInstance();
+			day.setTimeInMillis(getIntent().getExtras().getLong("DAY"));
+			setDay(day);
+			minHours = chk.unformatTotalHours(PreferencesActivity.getHoursPrefByDay(this, day
+					.get(Calendar.DAY_OF_WEEK)));
+		} else {
+			minHours = chk.unformatTotalHours(PreferencesActivity.getHoursPrefByDay(this, Calendar
+					.getInstance().get(Calendar.DAY_OF_WEEK)));
+		}
+		
+
+	}
+
+	@Override
+	protected void onResume() {
+		super.onResume();
+		if (getIntent().hasExtra("DAY")) {
+			Calendar day = Calendar.getInstance();
+			day.setTimeInMillis(getIntent().getExtras().getLong("DAY"));
+			setDay(day);
+			minHours = chk.unformatTotalHours(PreferencesActivity.getHoursPrefByDay(this, day
+					.get(Calendar.DAY_OF_WEEK)));
+		} else {
+			minHours = chk.unformatTotalHours(PreferencesActivity.getHoursPrefByDay(this, Calendar
+					.getInstance().get(Calendar.DAY_OF_WEEK)));
+		}
+	}
+
+	@Override
+	protected Dialog createAddDialog() {
+		Calendar c = Calendar.getInstance();
+		TimePickerDialog addDialog = new TimePickerDialog(this, onAddCheckpointListener, c
+				.get(Calendar.HOUR_OF_DAY), c.get(Calendar.MINUTE), true);
+		addDialog.setTitle(R.string.dialog_add_checkpoint_title);
+		return addDialog;
+	}
+
+	protected TimePickerDialog.OnTimeSetListener onAddCheckpointListener = new OnTimeSetListener() {
+
+		@Override
+		public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
+			Calendar cal = Calendar.getInstance();
+			cal.set(Calendar.HOUR_OF_DAY, hourOfDay);
+			cal.set(Calendar.MINUTE, minute);
+			insertCheckpoint(cal.getTimeInMillis());
+		}
+	};
+
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add(Menu.NONE, MENU_TODAY, Menu.NONE, R.string.tab_title_day).setIcon(
+				android.R.drawable.ic_menu_day);
+		return super.onCreateOptionsMenu(menu);
+
+	}
+
+	@Override
+	public boolean onMenuItemSelected(int featureId, MenuItem item) {
+		switch (item.getItemId()) {
+			case MENU_TODAY:
+				today = true;
+				fillData();
+				return true;
+			default:
+				return super.onMenuItemSelected(featureId, item);
+		}
+
+	}
+
+	public void setDay(Calendar day) {
+		this.day = day;
+		today = false;
+	}
+
+}
---
+++ tags/1.7/src/br/com/passeionaweb/android/hoursbank/HoursBank.java
@@ -0,0 +1,56 @@
+package br.com.passeionaweb.android.hoursbank;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.Menu;
+import android.widget.TabHost;
+import br.com.passeionaweb.android.hoursbank.BlotterActivity;
+import br.com.passeionaweb.android.hoursbank.DayActivity;
+import br.com.passeionaweb.android.hoursbank.MonthActivity;
+
+public class HoursBank extends TabActivity {
+	private static final String TAB_BLOTTER = "blotter";
+	private static final String TAB_DAY = "day";
+	private static final String TAB_MONTH = "month";
+
+	public static final int MENU_SETTINGS = 0;
+	public static final int MENU_EXPORT = 1;
+	public static final int MENU_DONATION = 2;
+	public static final String LINK_DONATION = "https://www.paypal.com/cgi-bin/webscr?cmd=_donations&business=H9FZXU9ADW5ZC&lc=BR&item_name=Rafael%20Roman&item_number=android%2dhoursbank&currency_code=USD&bn=PP%2dDonationsBF%3abtn_donateCC_LG%2egif%3aNonHosted";
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+
+		super.onCreate(savedInstanceState);
+
+		setupTabs();
+
+		PreferenceManager.setDefaultValues(this, R.xml.settings, false);
+
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add(Menu.NONE,MENU_EXPORT,Menu.NONE,R.string.menu_export).setIcon(android.R.drawable.ic_menu_share);
+		menu.add(Menu.NONE, MENU_SETTINGS, Menu.NONE, R.string.menu_settings).setIcon(android.R.drawable.ic_menu_preferences);
+		menu.add(Menu.NONE,MENU_DONATION,Menu.NONE,R.string.menu_donate).setIcon(R.drawable.donate);
+		return super.onCreateOptionsMenu(menu);
+	}
+
+	private void setupTabs() {
+		TabHost tabHost = getTabHost();
+		tabHost.addTab(tabHost.newTabSpec(TAB_DAY)
+		// TODO create an icon and set it here as a drawable
+				.setIndicator(getResources().getText(R.string.tab_title_day)).setContent(new Intent(this, DayActivity.class)));
+
+		tabHost.addTab(tabHost.newTabSpec(TAB_MONTH)
+		// TODO create an icon and set it here as a drawable
+				.setIndicator(getResources().getText(R.string.tab_title_month)).setContent(new Intent(this, MonthActivity.class)));
+
+		tabHost.addTab(tabHost.newTabSpec(TAB_BLOTTER).setIndicator(getResources().getText(R.string.tab_title_blotter), getResources().getDrawable(R.drawable.ic_tab_graph)).setContent(
+				new Intent(this, BlotterActivity.class)));
+	}
+}
