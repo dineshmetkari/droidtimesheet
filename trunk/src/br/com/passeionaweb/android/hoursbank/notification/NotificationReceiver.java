1558aa17e9ff143e3c32b6538f4e4fabfbf75447
panthro.rafael <panthro.rafael@bee5d759-e3f5-d037-51d3-742891c3956c>
2011-04-25 14:18:15 +0000

    Implementing the Notification for all events (update, insert, delete). Adding sound, vibrate and lights to notification.

--- trunk/src/br/com/passeionaweb/android/hoursbank/notification/NotificationReceiver.java
+++ trunk/src/br/com/passeionaweb/android/hoursbank/notification/NotificationReceiver.java
@@ -17,7 +17,14 @@ public class NotificationReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         if (ACTION_NOTIFICATION_ENDDAY.equals(intent.getAction())) {
             Notification notification = new Notification(R.drawable.black_clock, context.getText(R.string.notification_title_time_to_go), System.currentTimeMillis());
-            notification.flags += Notification.FLAG_AUTO_CANCEL + Notification.FLAG_SHOW_LIGHTS + Notification.FLAG_ONLY_ALERT_ONCE + Notification.DEFAULT_ALL;
+            notification.flags |= Notification.FLAG_AUTO_CANCEL;
+            notification.flags |= Notification.FLAG_SHOW_LIGHTS;
+            notification.flags |= Notification.FLAG_ONLY_ALERT_ONCE;
+            notification.defaults |= Notification.DEFAULT_SOUND;
+            notification.ledARGB = 0xff00ff00;
+            notification.ledOnMS = 300;
+            notification.ledOffMS = 1000;
+            notification.vibrate = new long[] { 0, 100, 200, 300 };
             Intent notificationIntent = new Intent(context, HoursBank.class);
             PendingIntent contentIntent = PendingIntent.getActivity(context, 0, notificationIntent, 0);
             notification.setLatestEventInfo(context, context.getText(R.string.notification_title_time_to_go), context.getText(R.string.notification_time_to_go), contentIntent);
