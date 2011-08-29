7c73a0e32fec3b1d1baa16656395c4e2bd4bf1fe
panthro.rafael <panthro.rafael@bee5d759-e3f5-d037-51d3-742891c3956c>
2011-08-29 21:45:19 +0000

    design changes

--- src/br/com/passeionaweb/android/hoursbank/HoursBank.java
+++ src/br/com/passeionaweb/android/hoursbank/HoursBank.java
@@ -6,8 +6,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.view.Menu;
+import android.view.Window;
 import android.widget.TabHost;
 import br.com.passeionaweb.android.billing.util.UpgradeChecker;
+import br.com.passeionaweb.android.hoursbank.design.TabHelper;
 
 public class HoursBank extends TabActivity {
     private static final String TAB_BLOTTER   = "blotter";
@@ -23,11 +25,11 @@ public class HoursBank extends TabActivity {
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
-
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
-
+        
         setupTabs();
-        UpgradeChecker.showUpgradeDialog(this);
+        UpgradeChecker.showUpgradeDialog(this); 
 
         PreferenceManager.setDefaultValues(this, R.xml.settings, false);
 
@@ -42,16 +44,28 @@ public class HoursBank extends TabActivity {
 
     private void setupTabs() {
         TabHost tabHost = getTabHost();
-        tabHost.addTab(tabHost.newTabSpec(TAB_DAY).setIndicator(getResources().getText(R.string.tab_title_day), getResources().getDrawable(R.drawable.ic_tab_day)).setContent(
-                new Intent(this, DayActivity.class)));
-        tabHost.addTab(tabHost.newTabSpec(TAB_WEEK).setIndicator(getString(R.string.tab_title_week), getResources().getDrawable(R.drawable.ic_tab_week)).setContent(
+        
+        TabHelper.addTab(tabHost, R.string.tab_title_day, TAB_DAY, R.drawable.ic_tab_day, new Intent(this, DayActivity.class));
+        
+        TabHelper.addTab(tabHost, R.string.tab_title_week, TAB_WEEK, R.drawable.ic_tab_week, new Intent(this, WeekActivity.class));
+        
+        
+        TabHelper.addTab(tabHost, R.string.tab_title_month, TAB_MONTH, R.drawable.ic_tab_month, new Intent(this, MonthActivity.class));
+        
+        TabHelper.addTab(tabHost, R.string.tab_title_blotter, TAB_BLOTTER, R.drawable.ic_tab_graph, new Intent(this, BlotterActivity.class));
+        
+        
+        /*tabHost.addTab(tabHost.newTabSpec(TAB_DAY).setIndicator(getResources().getText(R.string.tab_title_day), getResources().getDrawable(R.drawable.ic_tab_day)).setContent(
+                new Intent(this, DayActivity.class)));*/
+        
+        /*tabHost.addTab(tabHost.newTabSpec(TAB_WEEK).setIndicator(getString(R.string.tab_title_week), getResources().getDrawable(R.drawable.ic_tab_week)).setContent(
                 new Intent(this, WeekActivity.class)));
 
         tabHost.addTab(tabHost.newTabSpec(TAB_MONTH).setIndicator(getResources().getText(R.string.tab_title_month), getResources().getDrawable(R.drawable.ic_tab_month)).setContent(
                 new Intent(this, MonthActivity.class)));
 
         tabHost.addTab(tabHost.newTabSpec(TAB_BLOTTER).setIndicator(getResources().getText(R.string.tab_title_blotter), getResources().getDrawable(R.drawable.ic_tab_graph)).setContent(
-                new Intent(this, BlotterActivity.class)));
+                new Intent(this, BlotterActivity.class)));*/
     }
 
     @Override
